6fba8410d48a6e4b408ffa78d16aaa8a42b56ef2
colombovf <colombovf@gmail.com>
2022-11-17 17:37:44 +0100

    Fine Esercizio con bonus

--- script.js
+++ script.js
@@ -7,32 +7,32 @@ let ourTeam = [
     {
     nome: "Wayne Barnett",
     ruolo: "Founder & CEO",
-    foto: "wayne-barnett-founder-ceo.jpg"
+    foto: "<img src=./img/wayne-barnett-founder-ceo.jpg class=card-img-top foto>"
     },
     {
     nome: "Angela Caroll",
     ruolo: "Chief Editor",
-    foto: "angela-caroll-chief-editor.jpg"
+    foto: "<img src=./img/angela-caroll-chief-editor.jpg class=card-img-top foto>"
     },
     {
     nome: "Walter Gordon",
     ruolo: "attaccanOffice Managerte",
-    foto: "walter-gordon-office-manager.jpg"
+    foto: "<img src=./img/walter-gordon-office-manager.jpg class=card-img-top foto>"
     },
     {
     nome: "Angela Lopez",
     ruolo: "Social Media Manager",
-    foto: "angela-lopez-social-media-manager.jpg"
+    foto: "<img src=./img/angela-lopez-social-media-manager.jpg class=card-img-top foto>"
     },
     {
     nome: "Scott Estrada",
     ruolo: "Developer",
-    foto: "scott-estrada-developer.jpg"
+    foto: "<img src=./img/scott-estrada-developer.jpg class=card-img-top foto>"
     },
     {
     nome: "Barbara Ramos",
     ruolo: "Graphic Designer",
-    foto: "barbara-ramos-graphic-designer.jpg"
+    foto: "<img src=./img/barbara-ramos-graphic-designer.jpg class=card-img-top foto>"
     }
 ]
 
@@ -42,3 +42,19 @@ let ourTeam = [
 le informazioni di nome, ruolo e la stringa della foto */
 
 console.log(ourTeam);
+
+
+//Stampare le stesse informazioni su DOM sottoforma di stringhe.
+
+let nomi = document.getElementsByClassName("nome");
+let ruoli = document.getElementsByClassName("ruolo");
+let foto = document.getElementsByClassName("foto");
+
+
+for (let i = 0; i < ourTeam.length; i ++) {
+    nomi[i].innerHTML = ourTeam[i].nome;
+    foto[i].innerHTML = ourTeam[i].foto;
+    console.log(ourTeam[i].foto);
+    console.log(foto[i].innerHTML);
+    ruoli[i].innerHTML = ourTeam[i].ruolo;
+}
