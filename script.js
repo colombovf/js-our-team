ca6038d7597877037eec4fcc403cadfa5dfb8935
colombovf <colombovf@gmail.com>
2022-11-17 15:16:46 +0100

    Stampare a console l'array

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/*Utilizzando i dati forniti, creare un array di 
+oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti 
+informazioni: nome, ruolo e foto.*/
+
 let ourTeam = [  
     {
     nome: "Wayne Barnett",
@@ -30,3 +35,10 @@ let ourTeam = [
     foto: "barbara-ramos-graphic-designer.jpg"
     }
 ]
+
+
+
+/*Stampare su console, per ogni membro del team, 
+le informazioni di nome, ruolo e la stringa della foto */
+
+console.log(ourTeam);
